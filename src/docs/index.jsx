1549a9a87f438dc16923cf5c2fa48fe1f337900b
Ahmet Kapusuz <ahmetkapusuz@gmail.com>
2018-05-05 18:26:17 +0300

    Readme update

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
 import { render } from "react-dom";
 import CountdownTimer from "../../lib";
+import {addSeconds} from 'date-fns';
 import "./styles.css";
 
-class Demo() {
+class Demo extends Component {
 
   onTimerExpire() {
     console.log("Timer expired!");
   }
 
   render() {
+    const countdownDate = addSeconds(new Date(), 10);
+
     return (
       <div>
         <h1>Sample Timer Component</h1>
